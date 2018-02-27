a198348addab146d709691f97cb859f75de740be
Alex Orga <alexorga.dev@gmail.com>
2018-02-26 20:30:37 -0600

    completed exercise 2

--- strings.js
+++ strings.js
@@ -2,3 +2,9 @@
 
 var numOfSandwiches = 4 * 2;
 console.log('numOfSandwiches', numOfSandwiches);
+
+// Challenge #2
+
+var name = "Alex";
+var string = "Hello, " + name + " how are you today?";
+console.log('string', string); 
